4426818222d7b42ff7ca5b2a42f88a4308bb727d
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-22 15:04:19 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,8 @@ class Shoe
   BRANDS = []
 
   def initialize(brand)
-    this.brand=(brand)
+    @brand = brand
+    BRANDS << brand
   end
 
   def cobble
@@ -13,9 +14,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand=brand
-    BRANDS << brand
-  end
-
+  
 end
